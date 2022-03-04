a5aa06622d163599a00d0de735f3205a67a7c551
eurekasolution <nadopro@gmail.com>
2022-03-04 15:14:43 +0900

    2022-03-04 15:00

--- 24BookDB.py
+++ 24BookDB.py
@@ -21,5 +21,22 @@ def createTable():
     conn.commit()
     conn.close()
 
+def insertBooks():
+    conn = sqlite3.connect("book.db")
+    cur = conn.cursor()
+
+    sql = "insert into book_table (title, year, company, pages, exist_flag, price) " \
+          "values (?, ?, ?, ?, ?, ? )"
+    cur.execute(sql, ('자바', '2020', '하나은행', '300', '1', '12000') )
+
+    #cur.execute("insert into book_table (title, year, company, pages, exist_flag, price) "
+    #            "values ('파이썬', '2022', '국민은행', '1234', '1', '12000'  )")
+    conn.commit()
+
+    conn.close()
+
+
 if __name__ == "__main__":
-    createTable()
+    #createTable()
+    print("Insert...")
+    insertBooks()
