8fa933ed59b255799ce232dd6e15bfd4783788fb
eurekasolution <nadopro@gmail.com>
2022-04-05 13:35:52 +0900

    2022-04-05 1:35분

--- 39dbSelect.py
+++ 39dbSelect.py
@@ -6,9 +6,11 @@ try:
     client = MongoClient("mongodb://localhost:27017/")
 
     db = client['test']
-    print('저자', '제목', '종류')
+    print('저자\t\t', '제목\t', '종류')
     print('=' * 50)
-    for d in db['posts'].find(): #find()함수=select * from 테이블명
-         print(d['author'], d['title'], d['kind'])
+    for d in db['posts'].find():
+         print(d['author'], '\t', d['title'], '\t', d['kind'])
+
+    print('전체레코드갯수 =', db['posts'].estimated_document_count())
 except Exception as err:
     print('db조회에러발생 : ', err)
