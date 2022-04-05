2404cf6fb3284cb391981e3be8a6ad6fcdd66f18
eurekasolution <nadopro@gmail.com>
2022-04-05 11:43:45 +0900

    2022-04-05 1100

--- 38dbInsert.py
+++ 38dbInsert.py
@@ -9,10 +9,13 @@ try:
 
     db = client['test'] #데이터베이스생성
     print('test데이터베이스 생성 success 11:34')
-    data = { #data딕션=dict=딕트 미사용
+    data = {
         'author' : 'kim' ,
         'title' : 'apple' ,
         'kind' : 'red'
     }
+    db.posts.insert_one(data) #db.테이블명=Collection
+    print('데이터 한건 성공 ')
+    print('-' * 100)
 except Exception as err:
     print('db에러발생 : ', err)
